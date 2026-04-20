96bdaf36db3ab8d3ad9c2a194c2d4edbfe7b13df
jjr2024 <125372177+jjr2024@users.noreply.github.com>
2026-04-20 01:39:22 -0400

    react email fixes

--- src/emails/reminder.tsx
+++ src/emails/reminder.tsx
@@ -1,13 +1,11 @@
-import {
-  Html,
-  Head,
-  Body,
-  Container,
-  Section,
-  Text,
-  Link,
-  Hr,
-} from "react-email";
+import { Html } from "@react-email/html";
+import { Head } from "@react-email/head";
+import { Body } from "@react-email/body";
+import { Container } from "@react-email/container";
+import { Section } from "@react-email/section";
+import { Text } from "@react-email/text";
+import { Link } from "@react-email/link";
+import { Hr } from "@react-email/hr";
 import * as React from "react";
 
 interface GiftItem {
